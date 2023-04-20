3aa8b4acd98604ee6cf42e62f226e8d346e0a308
accedie-xcd <accedie.xcd@gmail.com>
2023-04-20 11:12:52 +0800

    test

---
+++ frontend/Homepage-Admin.js
@@ -0,0 +1,22 @@
+window.onload() = function () {
+  const artName = document.getElementById("article-name-5");
+
+  fetch('/api/articles/get', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        from: "Crimereads"
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+        artName.innerText = JSON.stringify(data);
+    })
+    .catch(error => {
+        console.error(error);
+        artName.innerText = 'An error occurred while fetching the data.';
+    });
+
+}
